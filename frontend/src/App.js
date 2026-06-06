30d442efcf4cffc37f755e815bfc8fe968275e9a
dilonsejdija75 <dilonsejdija75@gmail.com>
2026-06-06 01:28:42 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import WeatherApp from "@/pages/WeatherApp";
+import WeatherApp from "./pages/WeatherApp";  // ← Changed from "@/pages/WeatherApp"
 
 function App() {
   return (
